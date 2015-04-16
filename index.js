ab685aa1090ee8689862e62758b90da41a5cccf8
Mathieu Schroeter <schroeter@epsitec.ch>
2015-04-16 14:14:21 +0200

    Rewrite the daemon module with prototypes.
    I'ts much more readable.

--- index.js
+++ index.js
@@ -9,84 +9,95 @@ var xLog    = require ('xcraft-core-log') (moduleName);
 var xConfig = require ('xcraft-core-etc').load ('xcraft');
 
 
-module.exports = function (serverName, serverScript, detached) {
-  var proc = null;
+function Daemon (serverName, serverScript, detached) {
+  if (!(this instanceof Daemon)) {
+    return new Daemon (serverName, serverScript, detached);
+  }
 
-  var pidFile = path.join (xConfig.xcraftRoot, './var/run/' + serverName + 'd.pid');
+  this.serverName   = serverName;
+  this.serverScript = serverScript;
+  this.detached     = detached;
 
-  return {
-    start: function () {
-      var isRunning = false;
-      if (fs.existsSync (pidFile)) {
-        xLog.info ('the ' + serverName + ' server seems running');
+  this.proc    = null;
+  this.pidFile = path.join (xConfig.xcraftRoot, './var/run/' + serverName + 'd.pid');
+}
 
-        isRunning = true;
-        var pid = fs.readFileSync (pidFile, 'utf8');
+Daemon.prototype.start = function () {
+  var self = this;
 
-        try {
-          process.kill (pid, 0);
-        } catch (err) {
-          if (err.code === 'ESRCH') {
-            xLog.info ('but the process can not be found, then we try to start it');
-            fs.unlinkSync (pidFile);
-            isRunning = false;
-          }
-        }
+  var isRunning = false;
+  if (fs.existsSync (self.pidFile)) {
+    xLog.info ('the ' + self.serverName + ' server seems running');
+
+    isRunning = true;
+    var pid = fs.readFileSync (self.pidFile, 'utf8');
+
+    try {
+      process.kill (pid, 0);
+    } catch (err) {
+      if (err.code === 'ESRCH') {
+        xLog.info ('but the process can not be found, then we try to start it');
+        fs.unlinkSync (self.pidFile);
+        isRunning = false;
       }
-
-      if (!isRunning) {
-        var xProcess = require ('xcraft-core-process') ({logger: 'daemon'});
-
-        /* TODO: add logging capabilities. */
-        var options = {
-          detached: detached,
-          stdio:    detached ? 'ignore' : 'pipe'
-        };
-
-        var args = [serverScript];
-        if (process.env.hasOwnProperty ('XCRAFT_DEBUG') &&
-            parseInt (process.env.XCRAFT_DEBUG) === 1) {
-          args.unshift ('--debug');
-        }
-
-        proc = xProcess.spawn ('node', args, options, function (err) {
-          if (err) {
-            xLog.err (err);
-          }
-
-          fs.unlinkSync (pidFile);
-        });
-
-        xLog.info (serverName + ' server PID: ' + proc.pid);
-        fs.writeFileSync (pidFile, proc.pid);
-
-        if (detached) {
-          proc.unref ();
-        }
-      }
-    },
-
-    stop: function () {
-      try {
-        var pid = fs.readFileSync (pidFile, 'utf8');
-        process.kill (pid, 'SIGTERM');
-        try {
-          fs.unlinkSync (pidFile);
-        } catch (ex) {}
-      } catch (err) {
-        if (err.code !== 'ENOENT') {
-          xLog.err (err);
-        }
-      }
-    },
-
-    restart: function () {
-      this.stop ();
-      this.start ();
-    },
-
-    isOurDaemon: function () {
-      return proc && proc.pid;
     }
-  };
+  }
+
+  if (!isRunning) {
+    var xProcess = require ('xcraft-core-process') ({logger: 'daemon'});
+
+    /* TODO: add logging capabilities. */
+    var options = {
+      detached: self.detached,
+      stdio:    self.detached ? 'ignore' : 'pipe'
+    };
+
+    var args = [self.serverScript];
+    if (process.env.hasOwnProperty ('XCRAFT_DEBUG') &&
+        parseInt (process.env.XCRAFT_DEBUG) === 1) {
+      args.unshift ('--debug');
+    }
+
+    self.proc = xProcess.spawn ('node', args, options, function (err) {
+      if (err) {
+        xLog.err (err);
+      }
+
+      fs.unlinkSync (self.pidFile);
+    });
+
+    xLog.info (self.serverName + ' server PID: ' + self.proc.pid);
+    fs.writeFileSync (self.pidFile, self.proc.pid);
+
+    if (self.detached) {
+      self.proc.unref ();
+    }
+  }
 };
+
+Daemon.prototype.stop = function () {
+  var self = this;
+
+  try {
+    var pid = fs.readFileSync (self.pidFile, 'utf8');
+    process.kill (pid, 'SIGTERM');
+    try {
+      fs.unlinkSync (self.pidFile);
+    } catch (ex) {}
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      xLog.err (err);
+    }
+  }
+};
+
+Daemon.prototype.restart = function () {
+  this.stop ();
+  this.start ();
+};
+
+Daemon.prototype.isOurDaemon = function () {
+  return this.proc && this.proc.pid;
+};
+
+module.exports = Daemon;
